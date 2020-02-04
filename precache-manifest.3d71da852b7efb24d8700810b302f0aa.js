b4fafa4003eb748e5c5392a4202c294cf87b5740
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2020-02-04 12:30:17 -0500

    Updates

---
+++ precache-manifest.3d71da852b7efb24d8700810b302f0aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d45e5e0dcc439e2e55563151728ca9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1087f87b8551b2ca43c",
+    "url": "/static/css/main.43bf1d01.chunk.css"
+  },
+  {
+    "revision": "5b3ee323f0cddff86866",
+    "url": "/static/js/2.0da47c97.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0da47c97.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1087f87b8551b2ca43c",
+    "url": "/static/js/main.d82edf96.chunk.js"
+  },
+  {
+    "revision": "696942293a92ac3a2bb6",
+    "url": "/static/js/runtime-main.889ad4b6.js"
+  }
+]);
